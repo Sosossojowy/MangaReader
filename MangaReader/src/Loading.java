95de17793fc6ec74f7e251e9a79916569764c5ea
Sosossojowy <salatakarol@gmail.com>
2023-06-26 04:02:40 +0200

    comments in Loading.java

--- MangaReader/src/Loading.java
+++ MangaReader/src/Loading.java
@@ -11,6 +11,7 @@ public class Loading {
     private static final int MAX_THREADS = 10;
     private static final String SAVE_DIRECTORY = "C:\\Users\\{username}\\Pictures\\55\\";
 
+    // Delete a folder and its contents recursively
     public static void deleteFolder(File folder) {
         File[] files = folder.listFiles();
         if (files != null) {
@@ -24,6 +25,8 @@ public class Loading {
         }
         folder.delete();
     }
+
+    // Download images for a given chapter number
     public static void download(String chapterNumber) {
         try {
             int chapter = Integer.parseInt(chapterNumber);
@@ -33,10 +36,12 @@ public class Loading {
             ExecutorService executor = Executors.newFixedThreadPool(MAX_THREADS);
 
             while (i < 70) {
+                // Format chapter and page numbers with leading zeros
                 String.format(chapterNumber, "%02d");
                 String pageNumber = String.format("%02d", i + 1);
                 String link;
 
+                // Determine the image link based on chapter and page numbers
                 if (chapter < 100 & chapter <= 23) {
                     link = "https://official.lowee.us/manga/Dandadan/00" + chapterNumber + "-0" + pageNumber + ".png";
                 } else if (chapter < 100) {
@@ -45,6 +50,7 @@ public class Loading {
                     link = "https://scans-hot.leanbox.us/manga/Dandadan/0" + chapterNumber + "-0" + pageNumber + ".png";
                 }
 
+                // Asynchronously download and save the image
                 CompletableFuture.runAsync(() -> {
                     try {
                         URL url = new URL(link);
