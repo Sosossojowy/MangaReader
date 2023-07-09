8a82cfe6bf6ca6852b59c34041c4dd279baa0d79
Sosossojowy <salatakarol@gmail.com>
2023-07-09 13:54:20 +0200

    comms

--- MangaReader/src/Menu.java
+++ MangaReader/src/Menu.java
@@ -13,11 +13,13 @@ import java.util.Objects;
 import java.util.stream.IntStream;
 
 public class Menu extends JFrame {
-    private JComboBox<String> jComboBox1;
-
+    // Initial chapter count for the manga
     private final int initialChapterCount = 0;
+    // File name to store the progress
     private final String progressFileName = "chapter_progress.txt";
+    // Start date of the manga
     private final LocalDate startDate = LocalDate.of(2021, 5, 4);
+    private JComboBox<String> jComboBox1;
 
     public Menu() {
         initComponents();
@@ -40,6 +42,7 @@ public class Menu extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jPanel1.setBackground(new Color(255, 204, 204));
 
+        // Generate chapter numbers and populate the combo box
         String[] chapters = generateChapterNumbers(initialChapterCount);
         jComboBox1.setModel(new DefaultComboBoxModel<>(chapters));
         jComboBox1.addActionListener(this::imageDownload);
@@ -58,6 +61,7 @@ public class Menu extends JFrame {
         jLabel3.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/title.png"))));
         jLabel3.setText("jLabel3");
 
+        // Set up the layout using GroupLayout
         GroupLayout jPanel1Layout = new GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -121,43 +125,46 @@ public class Menu extends JFrame {
     }
 
     private void imageDownload(ActionEvent evt) {
+        // Get the username of the current user
         String username = System.getProperty("user.name");
+        // Get the selected chapter index from the combo box
         int chapterIndex = jComboBox1.getSelectedIndex();
+        // Format the chapter number with leading zeros
         String chapterNumber = String.format("%02d", chapterIndex + 1);
 
+        // Delete the existing folder and create a new one for image download
         Loading.deleteFolder(new File("C:\\Users\\" + username + "\\Pictures\\55"));
         new File("C:\\Users\\" + username + "\\Pictures\\55").mkdirs();
 
+        // Call the download method from the Loading class
         Loading.download(chapterNumber);
     }
 
     private void NewWindow(ActionEvent evt) {
+        // Open a new frame using the ConstructionWorkButton class
         ConstructionWorkButton.createFrame();
     }
 
     private void addChapterIfTuesday(ActionEvent evt) {
+        // Get the current date
         LocalDate currentDate = LocalDate.now();
+        // Count the number of Tuesdays passed since the start date
         int tuesdaysPassed = countTuesdaysPassed(startDate, currentDate);
+        // Calculate the current chapter count based on the initial count and Tuesdays passed
         int currentChapterCount = initialChapterCount + tuesdaysPassed;
+        // Set the current chapter count and update the combo box
         setCurrentChapterCount(currentChapterCount);
         updateChapterComboBox();
         System.out.println("Chapter added. Current count of chapters: " + currentChapterCount);
     }
 
     private int countTuesdaysPassed(LocalDate startDate, LocalDate endDate) {
+        // Count the number of weeks between start and end date, considering Tuesdays
         return (int) ChronoUnit.WEEKS.between(startDate, endDate.with(DayOfWeek.TUESDAY));
     }
 
-    private void setCurrentChapterCount(int currentChapterCount) {
-        try {
-            Path filePath = Path.of(progressFileName);
-            Files.writeString(filePath, String.valueOf(currentChapterCount), StandardOpenOption.CREATE);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     private void updateChapterComboBox() {
+        // Generate chapter numbers based on the current chapter count and update the combo box
         String[] chapters = generateChapterNumbers(getCurrentChapterCount());
         jComboBox1.setModel(new DefaultComboBoxModel<>(chapters));
     }
@@ -165,6 +172,7 @@ public class Menu extends JFrame {
     private int getCurrentChapterCount() {
         int currentChapterCount = initialChapterCount;
         try {
+            // Read the progress file to get the current chapter count
             Path filePath = Path.of(progressFileName);
             if (Files.exists(filePath)) {
                 String content = Files.readString(filePath);
@@ -176,7 +184,18 @@ public class Menu extends JFrame {
         return currentChapterCount;
     }
 
+    private void setCurrentChapterCount(int currentChapterCount) {
+        try {
+            // Write the current chapter count to the progress file
+            Path filePath = Path.of(progressFileName);
+            Files.writeString(filePath, String.valueOf(currentChapterCount), StandardOpenOption.CREATE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private String[] generateChapterNumbers(int chapterCount) {
+        // Generate an array of chapter numbers as strings
         return IntStream.rangeClosed(1, chapterCount)
                 .mapToObj(i -> String.format("%02d", i))
                 .toArray(String[]::new);
